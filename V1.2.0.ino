f2ac47288000f90fc3a937b52bfe67329763ab4b
Mahesh Pareek <155055272+Mahesh-pareek@users.noreply.github.com>
2024-06-11 14:19:13 +0530

    Update V1.2.0.ino

--- V1.2.0.ino
+++ V1.2.0.ino
@@ -11,6 +11,7 @@
 #include <WiFiClientSecure.h>
 #include <Arduino.h>
 #include <ESP_Google_Sheet_Client.h>
+#include <time.h>
 
 
 #define PROJECT_ID "biometric-attendance-device"
@@ -1279,6 +1280,20 @@ void setup() {
       delay(1);
     }
   }
+    // Initialize NTP
+  configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+  Serial.print("Waiting for NTP time sync: ");
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println();
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time: ");
+  Serial.print(asctime(&timeinfo));
   delay(3000);
   lcd.clear();
   lcd.setCursor(0, 0);
